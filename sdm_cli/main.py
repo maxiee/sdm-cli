fea97958dae6663403d1850e1a23ec4393bcc68c
maxiee <maxieewong@gmail.com>
2022-10-09 11:39:07 +0800

    calendar_update command

--- sdm_cli/main.py
+++ sdm_cli/main.py
@@ -1,2 +1,11 @@
+import fire
+from sdm_cli.constants import SDK
+
+
+class Command:
+    def calendar_update(self, sdk: SDK):
+        print('calendar_update')
+
 def main():
     print('hello sdm-cli')
+    fire.Fire(Command)
